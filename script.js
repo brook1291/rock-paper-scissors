62891199879ffc5fd32759d64930d3d5c29945f0
DOP <167078399+brook1291@users.noreply.github.com>
2024-08-17 10:55:06 +0100

    Add human choice logic and initialise scores

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+  if (randomNum === 1) {
+    return 'rock';
+  } else if (randomNum === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt(
+    "Type 'rock', 'paper', or 'scissors' to enter your choice."
+  );
+  if (
+    choice.toLowerCase() === 'rock' ||
+    choice.toLowerCase() === 'paper' ||
+    choice.toLowerCase() === 'scissors'
+  ) {
+    return choice.toLowerCase();
+  } else {
     const randomNum = Math.floor(Math.random() * 3) + 1;
     if (randomNum === 1) {
-        return "rock";
+      return 'rock';
     } else if (randomNum === 2) {
-        return "paper";
+      return 'paper';
     } else {
-        return "scissors";
+      return 'scissors';
     }
+  }
 }
+
+let humanScore = 0;
+let computerScore = 0;
