eba03902573eb143911d650d0715468320c32b33
DOP <167078399+brook1291@users.noreply.github.com>
2024-08-17 10:42:26 +0100

    Add computer choice logic

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+    if (randomNum === 1) {
+        return "rock";
+    } else if (randomNum === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
